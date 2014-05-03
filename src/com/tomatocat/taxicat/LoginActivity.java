8771ce7d435a8be26411a685bed55dbc8b16905a
akinachan <ars_reyes_fans@hotmail.com>
2014-05-03 22:13:04 +0700

    merge

--- src/com/tomatocat/taxicat/LoginActivity.java
+++ src/com/tomatocat/taxicat/LoginActivity.java
@@ -10,10 +10,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
